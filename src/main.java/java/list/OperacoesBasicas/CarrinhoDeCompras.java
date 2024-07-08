4f861d0ea947a7ca32bff839656bd126a90be2fc
Robert Dutra <robertdutra10@hotmail.com>
2024-07-08 18:35:20 -0300

    Desafio Carrinho de Compras
    Signed-off-by: Robert Dutra <robertdutra10@hotmail.com>

--- src/main.java/java/list/OperacoesBasicas/CarrinhoDeCompras.java
+++ src/main.java/java/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -1,4 +1,79 @@
 package list.OperacoesBasicas;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarrinhoDeCompras {
+
+    List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        Item item = new Item(nome, preco, quantidade);
+        itemList.add(item);
+    }
+
+    public void removerItem(String nome){
+        List<Item> list = new ArrayList<>();
+
+        if (!itemList.isEmpty()){
+            for (Item i : itemList) {
+                if (i.getNome().equalsIgnoreCase(nome)){
+                    list.add(i);
+                }
+            }
+            itemList.removeAll(list);
+        }else {
+            System.out.println("Lista vazia!");
+        }
+    }
+
+    public void calcularValorTotal(){
+
+        double total = 0d;
+
+        if (!itemList.isEmpty()){
+            for (Item item: itemList) {
+                total += item.getPreco() * item.getQuantidade();
+            }
+            System.out.printf("Valor total: %.2f%n", total);
+        }else {
+            System.out.println("Lista vazia!");
+        }
+
+    }
+
+    public void exibirItens(){
+        if (!itemList.isEmpty()) {
+            for (Item i :
+                    itemList) {
+                System.out.println("Produto: " +  i.getNome() + ", Precço: " + i.getPreco()
+                        + ", Quantidade: " + i.getQuantidade());
+            }
+        }else {
+            System.out.println("Lista vazia!");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        carrinhoDeCompras.adicionarItem("Celular", 5000d,2);
+        carrinhoDeCompras.adicionarItem("Celular", 3000d,1);
+        carrinhoDeCompras.adicionarItem("Capinha", 50d,5);
+        carrinhoDeCompras.adicionarItem("Carregador", 150d,3);
+        carrinhoDeCompras.exibirItens();
+        carrinhoDeCompras.removerItem("Celular");
+        carrinhoDeCompras.exibirItens();
+        carrinhoDeCompras.calcularValorTotal();
+
+
+
+    }
 }
--- src/main.java/java/list/OperacoesBasicas/Item.java
+++ src/main.java/java/list/OperacoesBasicas/Item.java
@@ -1,2 +1,39 @@
-package list.OperacoesBasicas;public class Item {
+package list.OperacoesBasicas;
+
+public class Item {
+
+    private String nome;
+    private Double preco;
+    private Integer quantidade;
+
+    public Item(String nome, Double preco, Integer quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
 }
